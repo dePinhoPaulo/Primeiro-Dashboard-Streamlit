1c89e09e3bf8fb67d7ffb8eaf27c7dc168d3ad37
dePinhoPaulo <kiko.sjs@gmail.com>
2026-07-10 21:52:47 -0300

    Adalises dos artilheiros

--- pagina_jogadores.py
+++ pagina_jogadores.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+# Dados
+df_jogadores = pd.read_csv('wc_top_scorers.csv')
+print(df_jogadores)
+
+df_paises_artilheiros = (
+    df_jogadores.groupby('country')['player']
+    .count()
+    .reset_index()
+    .sort_values(by='player', ascending=False)
+)
+
 # Pagina: Edições
 st.set_page_config(page_title="Jogadores", page_icon="🏆")
 #st.sidebar.header("Edições")
@@ -8,3 +19,27 @@ st.set_page_config(page_title="Jogadores", page_icon="🏆")
 # Painel
 st.title('Análise dos Jogadores')
 st.write('Maiores goleadores de Copas do Mundo:')
+
+st.text('')
+
+st.write('Atilheiros de cada edição: ')
+st.bar_chart(
+    data=df_jogadores,
+    x='player',
+    y='goals'
+)
+
+st.text('')
+
+st.write('Tabela completa: ')
+st.write(df_paises_artilheiros)
+
+st.text('')
+
+st.write('Pais com mais Atilheiros: ')
+st.bar_chart(
+    data=df_paises_artilheiros,
+    x='country',
+    y='player',
+    horizontal=True
+)
